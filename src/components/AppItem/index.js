817b3a79de9cf549d609d54bb6549567dbac229a
Chandu Munagapati <chandhu.munagapati@gmail.com>
2022-12-12 10:12:51 +0000

    app store

---
+++ src/components/AppItem/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const AppItem = props => {
+  const {appDetails} = props
+  const {appName, imageUrl} = appDetails
+
+  return (
+    <div className="app-item">
+      <img src={imageUrl} alt={appName} className="app-image" />
+      <p className="app-name">{appName}</p>
+    </div>
+  )
+}
+export default AppItem
---
+++ src/components/TabItem/index.js
@@ -0,0 +1,23 @@
+import './index.css'
+
+const TabItem = props => {
+  const {tabDetails, clickTabItem, isActive} = props
+  const {tabId, displayText} = tabDetails
+  const onClickTabItem = () => {
+    clickTabItem(tabId)
+  }
+
+  const tabBtnClassName = isActive ? 'tab-button active' : 'tab-button'
+  return (
+    <li className="tab-item">
+      <button
+        type="button"
+        className={tabBtnClassName}
+        onClick={onClickTabItem}
+      >
+        {displayText}
+      </button>
+    </li>
+  )
+}
+export default TabItem
